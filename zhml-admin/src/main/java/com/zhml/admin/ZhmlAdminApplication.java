02eb6596f16eb89be29d218677b16ffab8d6a12a
z85004510 <85004510@qq.com>
2019-09-19 17:48:05 +0800

    Initial commit

---
+++ zhml-admin/src/main/java/com/zhml/admin/ZhmlAdminApplication.java
@@ -0,0 +1,13 @@
+package com.zhml.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhmlAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhmlAdminApplication.class, args);
+    }
+
+}
---
+++ zhml-admin/src/main/java/com/zhml/admin/config/SpringConfig.java
@@ -0,0 +1,4 @@
+package com.zhml.admin.config;
+
+public class SpringConfig {
+}
